4f4c690521778f38b98c07bbf861067f065923c6
Severine Dubois <sevdbs@gmail.com>
2022-07-27 15:46:58 +0200

    coupon and couponstype entities created

---
+++ src/Entity/Coupon.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CouponRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CouponRepository::class)]
+class Coupon
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 15, unique: true)]
+    private ?string $code = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $discount = null;
+
+    #[ORM\Column]
+    private ?int $maxUsage = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $validity = null;
+
+    #[ORM\Column]
+    private ?bool $is_valid = null;
+
+    #[ORM\Column(options: ['default' => 'CURRENT_TIMESTAMP'])]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'coupons')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?CouponsType $couponsType = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDiscount(): ?int
+    {
+        return $this->discount;
+    }
+
+    public function setDiscount(int $discount): self
+    {
+        $this->discount = $discount;
+
+        return $this;
+    }
+
+    public function getMaxUsage(): ?int
+    {
+        return $this->maxUsage;
+    }
+
+    public function setMaxUsage(int $maxUsage): self
+    {
+        $this->maxUsage = $maxUsage;
+
+        return $this;
+    }
+
+    public function getValidity(): ?\DateTimeInterface
+    {
+        return $this->validity;
+    }
+
+    public function setValidity(\DateTimeInterface $validity): self
+    {
+        $this->validity = $validity;
+
+        return $this;
+    }
+
+    public function isIsValid(): ?bool
+    {
+        return $this->is_valid;
+    }
+
+    public function setIsValid(bool $is_valid): self
+    {
+        $this->is_valid = $is_valid;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getCouponsType(): ?CouponsType
+    {
+        return $this->couponsType;
+    }
+
+    public function setCouponsType(?CouponsType $couponsType): self
+    {
+        $this->couponsType = $couponsType;
+
+        return $this;
+    }
+}
