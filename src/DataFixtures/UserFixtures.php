44c797331caee609975dbc6b46d65e3dbbed2017
Severine Dubois <sevdbs@gmail.com>
2022-08-03 16:18:23 +0200

    user fixtures created

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class UserFixtures extends Fixture
+{
+
+    public function __construct(
+        private UserPasswordHasherInterface $passwordEncoder,
+        private SluggerInterface $slugger
+    )
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin =  new User();
+        $admin->setEmail('admin@admin.fr');
+        $admin->setLastname('Admin');
+        $admin->setFirstname('Administrateur');
+        $admin->setAddress('12 Rue du Port');
+        $admin->setZipcode('75001');
+        $admin->setCity('Paris');
+        $admin->setPassword(
+            $this->passwordEncoder->hashPassword($admin, 'admin')
+        );
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+
+        $faker = Faker\Factory::create('fr_FR');
+
+        for ($u = 1; $u <= 10; $u++){
+            $user = new User();
+            $user->setEmail($faker->email);
+            $user->setLastname($faker->lastName);
+            $user->setFirstname($faker->firstName);
+            $user->setAddress($faker->streetAddress);
+            $user->setZipcode(str_replace(' ', '', $faker->postcode));
+            $user->setCity($faker->city);
+            $user->setPassword(
+                $this->passwordEncoder->hashPassword($user, 'secret')
+            );
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+}
