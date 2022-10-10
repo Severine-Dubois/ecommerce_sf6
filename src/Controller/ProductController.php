60a0ebaf930749809d0cf9ae2d973624733edec4
Severine Dubois <sevdbs@gmail.com>
2022-10-10 13:52:50 +0200

    correcting code indentation + use data for product details page

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/produits', name: 'products')]
+#[Route('/produits', name: 'products_')]
 class ProductController extends AbstractController 
 {
     #[Route('/', name: 'index')]
