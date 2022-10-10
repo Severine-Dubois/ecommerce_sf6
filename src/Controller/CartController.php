572ca071417f4a9e58e91274af1395c786ad09db
Severine Dubois <sevdbs@gmail.com>
2022-10-10 17:19:28 +0200

    methods created in order to add, remove or delete products in cart + templates generated with data

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -12,9 +13,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class CartController extends AbstractController 
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(SessionInterface $session, ProductRepository $productRepository): Response
     {
-        return $this->render('cart/index.html.twig');
+        $panier = $session->get("panier", []);
+
+        // On "fabrique" les données
+        $dataPanier = [];
+        $total = 0;
+
+        foreach($panier as $id => $quantity) {
+            $product = $productRepository->find($id);
+            $dataPanier[] = [
+                "product" => $product,
+                "quantity" => $quantity,
+            ];
+
+            $total += $product->getPrice() * $quantity;
+        }
+
+        return $this->render('cart/index.html.twig', compact("dataPanier", "total"));
     }
 
     #[Route('/ajouter/{id}', name: 'add')]
@@ -23,6 +40,13 @@ class CartController extends AbstractController
         // On récupère le panier actuel
         $panier = $session->get("panier", []);
         $id = $product->getId();
+
+        // dans la session, la tableau du panier va se présenter sous cette forme :
+        //    [
+        //        "produit1" => quantité1,
+        //        "produit2" => quantité2
+        //    ]
+        // $panier[1] = quantité1
 
         if($id) {
             // si l'article existe déjà dans le panier
@@ -38,6 +62,58 @@ class CartController extends AbstractController
     
             return $this->redirectToRoute("cart_index");
         }
-
     }
+
+    #[Route('/retirer/{id}', name: 'remove')]
+    public function remove(Product $product, SessionInterface $session): Response
+    {
+        // On récupère le panier actuel
+        $panier = $session->get("panier", []);
+        $id = $product->getId();
+
+        if($id) {
+            // si l'article existe déjà dans le panier
+            if(!empty($panier[$id])) {
+                // et qu'il y en a + d'un
+                if($panier[$id] > 1) {
+
+                    $panier[$id]--;
+
+                } else {
+                    unset($panier[$id]);
+                }
+            } 
+            // On sauvegarde dans la session
+            $session->set("panier", $panier);
+    
+            return $this->redirectToRoute("cart_index");
+        }
+    }
+
+    #[Route('/supprimer/{id}', name: 'delete')]
+    public function delete(Product $product, SessionInterface $session): Response
+    {
+        // On récupère le panier actuel
+        $panier = $session->get("panier", []);
+        $id = $product->getId();
+
+        // si l'article existe déjà dans le panier
+            if($id && !empty($panier[$id])) {
+                unset($panier[$id]);
+            } 
+            // On sauvegarde dans la session
+            $session->set("panier", $panier);
+    
+            return $this->redirectToRoute("cart_index");
+        }
+
+        #[Route('/supprimer', name: 'delete_all')]
+        public function deleteAll(SessionInterface $session): Response
+        {
+            // Si on veut supprimer tous les articles du panier
+            //! Pas de session clear sinon on supprime tout, dont le session user
+            $session->remove("panier");
+        
+            return $this->redirectToRoute("cart_index");
+        }
 }
