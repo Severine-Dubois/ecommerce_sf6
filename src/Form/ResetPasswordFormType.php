b14e6f15d80e5e10d7603cb47220d86a65f0e36c
Severine Dubois <sevdbs@gmail.com>
2022-10-10 09:23:37 +0200

    reset password page + form created (via FormType)

---
+++ src/Form/ResetPasswordFormType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class ResetPasswordFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Entrez votre adresse mail',
+                'label_attr' => [
+                    'class' => 'block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300'
+                ],
+                'attr' => [
+                    'class' => 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white',
+                    'placeholder' => 'example@example.com',
+                ],
+            ]
+            );
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
