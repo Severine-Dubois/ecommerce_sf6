94f70a2c56b2f75b27bb761114302c7240c24e66
Severine Dubois <sevdbs@gmail.com>
2022-10-10 10:33:53 +0200

    use Forgot instead of Reset when the user have to submit their mail

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
-use App\Form\ResetPasswordFormType;
+use App\Form\ForgotPasswordFormType;
 use App\Repository\UserRepository;
 use App\Service\SendMailService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
@@ -50,7 +51,7 @@ class SecurityController extends AbstractController
         SendMailService $mail,
         ): Response
     {
-        $form = $this->createForm(ResetPasswordFormType::class);
+        $form = $this->createForm(ForgotPasswordFormType::class);
         $form->handleRequest($request);
 
         if($form->isSubmitted() && $form->isValid()) {
@@ -96,15 +97,29 @@ class SecurityController extends AbstractController
             return $this->redirectToRoute('login');
         }
 
-        return $this->render('security/reset_password_request.html.twig',
+        return $this->render('security/forgot_password.html.twig',
         [
-            'requestPassForm' => $form->createView(),
+            'forgotPassForm' => $form->createView(),
         ]);
     }
 
     #[Route(path: '/oubli-pass/{token}', name: 'reset_password')]
-    public function resetPassword(): Response
+    public function resetPassword(
+        string $token,
+        Request $request,
+        UserRepository $userRepository,
+        EntityManagerInterface $em,
+        UserPasswordHasherInterface $passwordHasher,
+    ): Response
     {
-        // TODO : create Response
+        // On vérifie si on a ce token dans la BDD
+        $user = $userRepository->findOneByResetToken($token);
+
+        if($user) {
+
+        }
+
+        $this->addFlash('red', 'Un problème est survenu');
+        return $this->redirectToRoute('login');
     }
 }
